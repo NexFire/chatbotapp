eae5050d7c04e9761c530d263400dda539785337
NexFire <45328192+NexFire@users.noreply.github.com>
2023-07-25 08:47:08 +0200

    Add files via upload
    This is the main file

---
+++ chatbots.py
@@ -0,0 +1,36 @@
+import openai
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget,QLineEdit,QScrollArea,QVBoxLayout,QHBoxLayout,QListWidget,QListWidgetItem
+from PyQt5.QtCore import Qt, QSize
+import sys
+openai.api_key="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"
+
+
+response=openai.ChatCompletion.create(model="gpt-3.5-turbo",
+  messages=[
+        {"role":"system","content":"Your are to be behaving like a normal person in its teen years. Respond in a way that coresponds to that"},
+        {"role": "user", "content": "how are you?"},
+    ])
+
+def main():
+    app = QApplication([])
+    window = QWidget()
+    leftMenuScroll=QScrollArea()
+    leftMenuWidget=QListWidget()
+    leftMenuLayout=QVBoxLayout()
+    mainLayout=QHBoxLayout()
+    items=[]
+    for i in range(1,50):
+        items.append(QListWidgetItem(str(i)))
+        leftMenuWidget.addItem(items[-1])
+
+    window.setWindowTitle("Chat Bot's")
+    window.setGeometry(100, 100, 280, 80)
+    leftMenuScroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+    leftMenuScroll.setWidget(leftMenuWidget)
+
+    mainLayout.addWidget(leftMenuWidget)
+    window.setLayout(mainLayout)
+    window.show()
+    sys.exit(app.exec())
+
+main()
